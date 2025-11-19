9fd1145ff8eb8398eb0cee3282c614a119c30c1a
cassiuste <cassiustedesco1@gmail.com>
2025-11-19 16:45:06 +0100

    Created the view package

--- app/src/main/java/com/example/frontendnursesapplication/views/Home.kt
+++ app/src/main/java/com/example/frontendnursesapplication/views/Home.kt
@@ -8,14 +8,11 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun Home(int: Int){
-    Row(modifier = Modifier.size(50.dp)){
-        Text(text = int.toString())
-        Text(text = "El 5 es un miercoles")
-    }
+
 }
 
 @Preview(showBackground = true)
 @Composable
 fun HomePreview(){
-    Home(5)
+
 }
